2cc9a159e0b2aa157f517a4584de55f74d348ba1
greatghoul <greatghoul@gmail.com>
2013-04-09 22:06:13 +0800

    Fix gist slideshow, remove slideN mode

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from flask import Flask, abort, request, redirect, render_template, url_for
 from log import log
-import util
 import os
 import re
 import base64
@@ -15,33 +14,49 @@ gh = GitHub()
 def home():
     log.info('Fetching demo gist.')
     gist_id = '5123482'
-    gist = gh.gists(gist_id).get()
-    source = util.get_slides_source_from_gist(gist)
 
-    return render_template('index.html', gist_id=gist_id, source=source)
+    return render_template('index.html', gist_id=gist_id)
 
-@app.route('/gist/', methods=['GET'])
-@app.route('/gist/<gist_id>/', methods=['GET'])
-def play_gist(gist_id=None):
-    # Fix url to a restful style.
-    if gist_id is None:
-        if 'gist_id' in request.args:
-            return redirect(url_for('play_gist', gist_id=request.args['gist_id']))
-        else:
-            abort(404)
-    else:
-        log.info('Creating slides from gist: %s' % gist_id)
-        gist = gh.gists(gist_id).get()
-        if gist is None:
-            abort(404)
-        
-        title = gist.get('description', 'Remarks')
-        source = util.get_slides_source_from_gist(gist)
-        return render_template('slideshow.html', title=title, source=source)
 
-def _repo_slides(resp):
+def _gist_slides(gist):
     slides = dict(title=u'Remarks Slides')
-    content = base64.b64decode(resp.get('content', '')).decode('utf-8')
+    content = gist.get('files', {}).get('slides.md', {}).get('content', '')
+    for line in re.sub(r'\s*^---.*$[\s\S]*', '', content, flags=re.M).split('\n'):
+        key, val = re.split(r':\s*', line, maxsplit=1)
+        slides[key] = val
+    slides['content'] = content
+    return slides
+
+@app.route('/gist/<gist_id>/', methods=['GET'])
+@app.route('/gist/<gist_id>/<filename>', methods=['GET'])
+def gist_file(gist_id, filename='slides.md'):
+    try:
+        log.info('Fetching gist %s content: %s', gist_id, filename)
+        gist = gh.gists(gist_id).get()
+        if 'raw' not in request.args and filename == 'slides.md':
+            slides = _gist_slides(gist)
+            return render_template('slideshow.html', slides=slides)
+        else:
+            if request.args.get('raw') == '1':
+                return '<pre>' + gist.get('files', {}).get('slides.md', {}).get('content', '') + '</pre>'
+            else:
+                raw_url = gist.get('files', {}).get(filename, {}).get('raw_url')
+                log.info('  Raw url: %s', raw_url)
+                return redirect(raw_url)
+
+    except ApiNotFoundError, e:
+        log.error(e.response)
+    except ApiError, e:
+        log.error(e.response)
+    except Exception, e:
+        log.error(e)
+
+    return abort(404)
+
+
+def _repo_slides(repo):
+    slides = dict(title=u'Remarks Slides')
+    content = base64.b64decode(repo.get('content', '')).decode('utf-8')
     for line in re.sub(r'\s*^---.*$[\s\S]*', '', content, flags=re.M).split('\n'):
         key, val = re.split(r':\s*', line, maxsplit=1)
         slides[key] = val
@@ -57,12 +72,15 @@ def repo_file(owner, repo, path, filename='slides.md'):
     try:
         branch = request.args.get('branch', 'master')
         log.info('%s/%s/%s/%s', owner, repo, path, filename)
-        if filename == 'slides.md':
-            resp = gh.repos(owner)(repo).contents(path + '/' + filename).get(ref=branch)
-            slides = _repo_slides(resp)
+        repo = gh.repos(owner)(repo).contents(path + '/' + filename).get(ref=branch)
+        if 'raw' not in request.args and filename == 'slides.md':
+            slides = _repo_slides(repo)
             return render_template('slideshow.html', slides=slides)
         else:
-            return redirect(_repo_attach(owner, repo, branch, path, filename))
+            if request.args.get('raw') == '1':
+                return base64.b64decode(repo.get('content', '')).decode('utf-8')
+            else:
+                return redirect(_repo_attach(owner, repo, branch, path, filename))
 
     except ApiNotFoundError, e:
         log.error(e.response)
