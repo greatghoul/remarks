1660ac7c35a42cb7476376ff9df214b4acb78135
greatghoul <greatghoul@gmail.com>
2013-04-16 11:55:18 +0800

    Update bookmarklet

--- templates/bookmarklet.js
+++ templates/bookmarklet.js
@@ -9,7 +9,7 @@
     if (hostname === 'gist.github.com' && (match = path.match(GIST_FILE_PATTERN))) {
         slides = slides + 'gist/' + match[2]; 
     } else if ((hostname === 'www.github.com' || hostname === 'github.com') && (match = path.match(REPO_FILE_PATTERN))) {
-        slides = slides + 'repo/' + match[1] + '/' + match[2] + '/' + match[4] + '?branch=' + match[3]; 
+        slides = slides + 'repo/' + match[1] + '/' + match[2] + '/' + match[4] + '/?branch=' + match[3]; 
     } else {
         alert('Not a valid remarks slides'); 
     }
