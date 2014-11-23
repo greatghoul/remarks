91bdd7b1fc019f08444aa848b2de06cc785bee95
greatghoul <greatghoul@gmail.com>
2014-11-23 22:21:52 +0800

    extract source info

--- app/utils/github_utils.py
+++ app/utils/github_utils.py
@@ -3,8 +3,8 @@
 
 import re
 
-PATTERN_REPO_URL = r'(?P<user>[^\/]+)\/(?P<repo>[^\/]+)\/(?P<path>[^\/]+)'
-PATTERN_GIST_URL = r'(?P<user>[^\/]+)\/(?P<repo>[^\/]+)'
+PATTERN_REPO_URL = r'(?P<user>[^\/]+)\/(?P<repo>[^\/]+)\/(?P<path>\S+)'
+PATTERN_GIST_URL = r'(?P<user>[^\/]+)\/(?P<gist>[^\/]+)'
 
 def source_type(path):
     """ Detect if a given path stands for a repo path or a gist path
@@ -15,3 +15,18 @@ def source_type(path):
         return 'gist'
     else:
         return None
+
+def source_info(path):
+    """ Extract source info from given path
+
+    If given a repo path, returns user, repo and path in hash
+    If given a gist path, returns user, gist in hash
+    """
+
+    the_source_type = source_type(path)
+    if the_source_type == 'repo':
+        return re.match(PATTERN_REPO_URL, path).groupdict()
+    elif the_source_type == 'gist':
+        return re.match(PATTERN_GIST_URL, path).groupdict()
+
+    return {}
--- tests/test_github_utils.py
+++ tests/test_github_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from utils.github_utils import source_type
+from utils.github_utils import source_type, source_info
 
 class TestGithubUtils(TestCase):
 
@@ -8,3 +8,14 @@ class TestGithubUtils(TestCase):
         self.assertEqual(source_type('greatghoul/slides/remarks'), 'repo')
         self.assertEqual(source_type('greatghoul/slides/remarks/v1'), 'repo')
         self.assertEqual(source_type('greatghoul/63732627262'), 'gist')
+
+    def test_get_repo_info_from_given_path(self):
+        repo = source_info('greatghoul/slides/remarks')
+        self.assertEqual(repo['user'], 'greatghoul')
+        self.assertEqual(repo['repo'], 'slides')
+        self.assertEqual(repo['path'], 'remarks')
+
+        repo = source_info('greatghoul/slides/remarks/v1')
+        self.assertEqual(repo['user'], 'greatghoul')
+        self.assertEqual(repo['repo'], 'slides')
+        self.assertEqual(repo['path'], 'remarks/v1')
